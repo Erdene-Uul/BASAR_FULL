61ddd909a9255121d4b8613156daa5385746bbfd
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-03-01 14:37:00 +0800

    blog photo changed

--- bsrr/src/pages/Blog/index.js
+++ bsrr/src/pages/Blog/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import Photo from "../../assets/images/pages/notfound.png";
+import blog from "../../assets/images/pages/blog.png";
 class Blog extends Component {
     render() {
         return (
             <div>
-                <img src={Photo} alt="notFound"/>
+                <img src={blog} alt="blog"/>
             </div>
         );
     }
