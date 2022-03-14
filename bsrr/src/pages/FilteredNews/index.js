afb7bfabe8d57845c6c357ff5e728990a27529cf
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-03-14 17:35:59 +0800

    zaswar1

--- bsrr/src/pages/FilteredNews/index.js
+++ bsrr/src/pages/FilteredNews/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import Content from '../../components/content';
 import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
-import { useHistory } from 'react-router-dom';
 
 const FilteredNews = (props) => {
 
@@ -11,10 +10,6 @@ const FilteredNews = (props) => {
 
     const filtered = props.news.filter(el => el.title.toLowerCase().includes(props.location.state))
 
-    const history = useHistory('');
-    if (!props.location.state) {
-        history.push('/')
-    }
     return (
         <div className='m-20 flex flex-col justify-center items-center space-y-10'>
             <h1 className='font-bold'>{`Хайлтын илэрц: ${filtered.length}`}</h1>
